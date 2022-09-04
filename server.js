809011f8ca922e7d511d8de40bf8d33e19a06a68
antieatingactivist <antieatingactivist@gmail.com>
2022-09-03 18:06:36 -0700

    fixed .env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const helpers = require('./utils/helpers');
-
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -35,7 +35,7 @@ app.use(require('./controllers'));
 
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 });
 
 
